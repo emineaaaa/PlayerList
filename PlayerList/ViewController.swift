594869d7614dcfc69ce8de45c0749a8050fc4c1b
emineaaaa <emine0aydinli3@gmail.com>
2023-08-09 22:42:17 +0300

    Initial Commit

---
+++ PlayerList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayerList
+//
+//  Created by Emine Aydınlı on 9.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
